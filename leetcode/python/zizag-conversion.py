43bff013526a1f97df25467b06d3ba9a96ac2495
Vlad Gheorghe <contact@vlad-gheorghe.com>
2022-08-17 08:12:39 +0200

    new challenge

---
+++ leetcode/python/zizag-conversion.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/zigzag-conversion/
+
+# Runtime: 63 ms, faster than 89.83% of Python3 online submissions for Zigzag Conversion.
+# Memory Usage: 14.1 MB, less than 51.21% of Python3 online submissions for Zigzag Conversion.
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        result = [''] * numRows
+        index, step = 0, 1
+        
+        for char in s:
+            result[index] += char
+        
+            if index == 0:
+                step = 1
+
+            if index == numRows - 1:
+                step = -1
+
+            index += step
+        
+        return ''.join(result)
